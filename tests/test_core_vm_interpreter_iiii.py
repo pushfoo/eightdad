36d1532d7499434869af8040b24bbaafc2ffa97c
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-01 04:54:06 -0400

    Add test for 00EE return instruction

---
+++ tests/test_core_vm_interpreter_iiii.py
@@ -0,0 +1,25 @@
+"""
+
+Tests for iiii instructions
+
+
+"""
+
+import pytest
+
+from eightdad.core import Chip8VirtualMachine as VM
+from eightdad.core.vm import DEFAULT_EXECUTION_START
+from tests.util import load_and_execute_instruction
+
+
+@pytest.mark.parametrize("call_location", (0xF00, 0x500))
+def test_00ee_returns_to_last_location(call_location):
+    vm = VM()
+    vm.stack_call(call_location)
+
+    load_and_execute_instruction(
+        vm,
+        0x00EE, # return instruction
+        load_point=0xF00
+    )
+    assert vm.stack_top == DEFAULT_EXECUTION_START
