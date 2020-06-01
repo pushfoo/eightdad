f5637b1b493e3f5f2dc9860d0a0c2bdc2e96bd23
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-01 03:45:34 -0400

    Move non-program memory locations to annn test

--- tests/test_core_vm_interpreter_innn.py
+++ tests/test_core_vm_interpreter_innn.py
@@ -3,7 +3,7 @@
 Tests for the innn instructions:
 
 * 1nnn, jump to nnn: set pc to nnn
-* 2nn, call routine at nnn: push current location to stack, jump to nnn
+* 2nnn, call routine at nnn: push current location to stack, jump to nnn
 * Annn, set I to nnn
 * Bnnn, Jump to nnn + v0: set pc to nnn + v0
 
@@ -14,19 +14,18 @@ from eightdad.core import Chip8VirtualMachine as VM
 from tests.util import load_and_execute_instruction
 
 VALID_MEMORY_LOCATIONS = (
-    0x0,
-    0x1,
     0x200,
     0xAAA,
     0xFFF
 )
 
-
-@pytest.mark.parametrize( "memory_location", VALID_MEMORY_LOCATIONS)
+@pytest.mark.parametrize(
+    "memory_location",
+    (0x0, 0x1) + VALID_MEMORY_LOCATIONS
+)
 def test_annn_sets_i_register(memory_location):
     """Annn instructions set the I register"""
     vm = VM()
     assert vm.i_register == 0
     load_and_execute_instruction(vm, 0xA000, nnn=memory_location)
     assert vm.i_register == memory_location
-
