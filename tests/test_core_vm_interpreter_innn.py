11141d1d8de94b7b8f28486f7832742898247afe
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-01 03:49:26 -0400

    Add tests for 2nnn call instruction

--- tests/test_core_vm_interpreter_innn.py
+++ tests/test_core_vm_interpreter_innn.py
@@ -11,6 +11,7 @@ Tests for the innn instructions:
 import pytest
 
 from eightdad.core import Chip8VirtualMachine as VM
+from eightdad.core.vm import DEFAULT_EXECUTION_START
 from tests.util import load_and_execute_instruction
 
 VALID_MEMORY_LOCATIONS = (
@@ -29,3 +30,16 @@ def test_annn_sets_i_register(memory_location):
     assert vm.i_register == 0
     load_and_execute_instruction(vm, 0xA000, nnn=memory_location)
     assert vm.i_register == memory_location
+
+
+@pytest.mark.parametrize("memory_location", VALID_MEMORY_LOCATIONS)
+def test_2nnn_sets_program_counter_and_pushes_stack(memory_location):
+    """2nnn instruction jumps to location and increments stack"""
+    vm = VM()
+
+    assert vm.program_counter == DEFAULT_EXECUTION_START
+    load_and_execute_instruction(vm, 0x2000, nnn=memory_location)
+    assert vm.program_counter == memory_location
+    assert vm.call_stack[-1] == DEFAULT_EXECUTION_START
+
+
