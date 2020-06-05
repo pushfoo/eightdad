8b2f5e1423caf6f1bc7d9168110118af7aef7682
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-05 17:42:35 -0400

    Add test for unrecognized well-patterned opcodes

---
+++ tests/core/vm/interpreter/test_unhandled_instruction.py
@@ -0,0 +1,23 @@
+import pytest
+from eightdad.core import Chip8VirtualMachine
+from tests.util import load_and_execute_instruction
+
+
+@pytest.mark.parametrize(
+    "patterned_unrecognizeable_instruction",
+    (
+        0xFFFF,
+        0xE1FF,
+        0x5001,
+    )
+)
+def test_valuerror_on_unhandled_instruction(
+        patterned_unrecognizeable_instruction
+):
+    """Unhandled instructions raise ValueError"""
+    vm = Chip8VirtualMachine()
+    with pytest.raises(ValueError):
+        load_and_execute_instruction(
+            vm,
+            patterned_unrecognizeable_instruction
+        )
