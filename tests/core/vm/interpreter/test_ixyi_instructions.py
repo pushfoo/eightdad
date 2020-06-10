4df51f967b0588b28011426d701f238115a77a75
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-10 10:25:03 -0400

    Add tests for 8xy7, vx = vy - vx

--- tests/core/vm/interpreter/test_ixyi_instructions.py
+++ tests/core/vm/interpreter/test_ixyi_instructions.py
@@ -402,3 +402,68 @@ class Test8XY6SetsVxToVyShiftedRight1:
         touched = {x, 0xF}
 
         assert other_registers_untouched(vm, touched)
+
+
+class Test8XY7SetsVxToVyMinusVx:
+
+    # Avoiding setting VF as the destination is intentional. Any
+    # difference written to VF will be overridden with a bit flag.
+    @pytest.mark.parametrize("x", range(0, 0xF))
+    @pytest.mark.parametrize("y", range(0, 16))
+    @pytest.mark.parametrize("a", (1, 2))
+    @pytest.mark.parametrize("b", (1, 0))
+    def test_8xy7_sets_vx_to_vy_minus_vx(self, x, y, a, b):
+        """8xy7 sets VX = VY - VX, clamped to 0 minimum"""
+
+        vm = VM()
+
+        vm.v_registers[x] = a
+
+        if x != y:
+            vm.v_registers[y] = b
+
+        load_and_execute_instruction(vm, 0x8007, x=x, y=y)
+
+        if x != y:
+            assert vm.v_registers[x] == max(b - a, 0)
+
+        else:  # any value minus itself yields zero
+            assert vm.v_registers[x] == 0
+
+    @pytest.mark.parametrize("x", range(0, 16))
+    @pytest.mark.parametrize("y", range(0, 16))
+    @pytest.mark.parametrize("a", (1, 255))
+    @pytest.mark.parametrize("b", (1,))
+    def test_8xy7_sets_vf_to_not_borrow(self, x, y, a, b):
+        """8xy7 sets VF to 1 if VX >= VY, otherwise 0"""
+        vm = VM()
+
+        vm.v_registers[x] = a
+        if x != y:
+            vm.v_registers[y] = b
+
+        load_and_execute_instruction(vm, 0x8007, x=x, y=y)
+
+        assert vm.v_registers[0xF] == int(b >= a)
+
+    @pytest.mark.parametrize("x", range(0, 16))
+    @pytest.mark.parametrize("y", range(0, 16))
+    def test_8xy7_leaves_other_registers_alone_unless_theyre_vf(
+        self,
+        x,
+        y
+    ):
+        """8xy5 leaves registers other than VX and VY alone except for VF"""
+
+        vm = VM()
+
+        vm.v_registers[x] = 1
+        vm.v_registers[y] = 2
+
+        load_and_execute_instruction(vm, 0x8007, x=x, y=y)
+
+        # make sure we don't check VF since it should always be set
+        touched = {x, 0xF}
+
+        assert other_registers_untouched(vm, touched)
+
