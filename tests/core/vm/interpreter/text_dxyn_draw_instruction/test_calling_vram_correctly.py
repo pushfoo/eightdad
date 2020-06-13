e7177c4fcc8a0dc7fed655dc6de7a7db55cbbec4
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-13 04:15:10 -0400

    Parametrize DXYN whitebox test of vram call

--- tests/core/vm/interpreter/text_dxyn_draw_instruction/test_calling_vram_correctly.py
+++ tests/core/vm/interpreter/text_dxyn_draw_instruction/test_calling_vram_correctly.py
@@ -5,20 +5,31 @@ from eightdad.core import VideoRam
 from eightdad.core.vm import Chip8VirtualMachine as VM
 from tests.util import load_and_execute_instruction
 
-def test_dxyn_calls_videoram_draw_correctly():
-    """DXYN args are correctly passed to VideoRam draw call"""
+@pytest.mark.parametrize(
+    "x,y,n,i",
+    (
+        (1, 2, 1, 0xA00),
+        (0, 1, 2, 0xB00)
+    )
+)
+def test_dxyn_calls_videoram_draw_correctly(x,y,n,i):
+    """DXYN args are correctly passed to VideoRam draw call
+
+    I'm not actually sure that this is a great idea. Need a more
+    experienced developer to give feedback on it.
+    """
     vm = VM()
     vm.video_ram = Mock(VideoRam)
     vm.video_ram.draw_sprite.return_value = 1
 
-    vm.v_registers[1] = 1
-    vm.v_registers[2] = 2
+    vm.v_registers[x] = x
+    vm.v_registers[y] = y
 
-    vm.i_register = 0xA00
+    vm.i_register = i
     load_and_execute_instruction(
-        vm, 0xD123
+        vm, 0xD000, x=x, y=y, n=n
     )
     assert vm.video_ram.draw_sprite.called_with(
-        1, 2, vm.memory, num_bytes=3, offset=0xA00
+        x, y, vm.memory, num_bytes=n, offset=i
     )
 
