1fe8b17a386daac13ae208ae9ca6dcddcb5b6a73
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-17 05:40:48 -0400

    Add tests for video_ram_type arg to VM

--- tests/core/vm/interpreter/test_vm_init.py
+++ tests/core/vm/interpreter/test_vm_init.py
@@ -1,6 +1,7 @@
 import pytest
 from itertools import product
-from eightdad.core import Chip8VirtualMachine
+from eightdad.core import Chip8VirtualMachine, VideoRam
+
 
 @pytest.mark.parametrize(
     "ram_size",
@@ -16,3 +17,30 @@ def test_ram_size(ram_size):
     vm = Chip8VirtualMachine(memory_size=ram_size)
     print(vm.memory)
     assert len(vm.memory) == ram_size
+
+
+class VRamSubclass(VideoRam):
+    """Dummy that makes sure subclasses work as VideoRam type args"""
+    pass
+
+
+class BadClass:
+    """Not a subclass of VideoRam"""
+    pass
+
+
+class TestVideoRamArg:
+
+    @pytest.mark.parametrize("vram_type", (VideoRam, VRamSubclass))
+    def test_vm_raises_no_error_on_good_types(self, vram_type):
+        vm = Chip8VirtualMachine(video_ram_type=vram_type)
+
+    def test_typeerror_on_bad_vram_type(self):
+        """Non-VideoRam class passed in video_ram_type"""
+        with pytest.raises(TypeError):
+            vm = Chip8VirtualMachine(video_ram_type=BadClass)
+
+    def test_typeerror_on_not_a_type(self):
+        """Non-type passed in video_ram_type"""
+        with pytest.raises(TypeError):
+            vm = Chip8VirtualMachine(video_ram_type="not a type")
