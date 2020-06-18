21563f7a39356cab29f4d145ab5a17655fd29245
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-06-18 18:12:32 -0400

    Make tests run again on recent libs, closes #40

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+"""
+This is a hacky stub that makes tests work. Why? Not yet sure. ¯\_(ツ)_/¯
+A python discord user suggested this fix, and didn't elaborate.
+
+In the past, tests ran fine with util.py being auto-imported from the
+tests dir.
+
+When I upgraded pytest or one of the other libraries (pip, setuptools, ?)
+to get current arcade versions to work, it appears to have changed how
+import works. Adding this file to turn tests into a module works even if
+I'm not exactly sure why.
+
+"""
