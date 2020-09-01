c64696fd215d8802880ff052f2da7eb26ab21722
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-09-01 06:14:56 +0000

    Add test for fx0a pausing execution, stub others

--- tests/core/vm/interpreter/test_keys.py
+++ tests/core/vm/interpreter/test_keys.py
@@ -1,6 +1,6 @@
 import pytest
 from eightdad.core import Chip8VirtualMachine as VM
-from tests.util import load_and_execute_instruction
+from tests.util import load_and_execute_instruction as load_and_execute
 
 
 class TestStateSet:
@@ -25,4 +25,26 @@ class TestStateSet:
         for key in range(0,16):
             vm.release(key)
             assert not vm.pressed(key)
+
+
+class TestFX0APauseTillAnyKeypress:
+    """
+    Tests for waiting for a key press
+    """
+
+    def test_fx0a_pauses_execution(self, x=0):
+        """Ticking the VM after fx0a is executed does not advance state"""
+        vm = VM()
+        start_position = vm.program_counter
+        load_and_execute(vm, 0xF00A,x=x)
+        vm.tick(1/20.0)
+        assert vm.program_counter == start_position
+
+    def test_fx0a_resumes_after_keypress(self):
+        """Execution is resumed after fx0a after a key is pressed"""
+        assert False
+
+    def test_fx0a_sets_register_targeted(self):
+        """A keypress while waiting sets the register to the requested state"""
+        assert False
 
