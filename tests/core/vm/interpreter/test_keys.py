6459ac9d39abfc8f14d73eea1fa5e0cbb4d9ceb3
pushfoo <36696816+pushfoo@users.noreply.github.com>
2020-09-04 09:46:21 +0000

    Refactor FX0A tests to be shorter and use load_multiple

--- tests/core/vm/interpreter/test_keys.py
+++ tests/core/vm/interpreter/test_keys.py
@@ -1,8 +1,11 @@
 import pytest
 from itertools import product
 from eightdad.core import Chip8VirtualMachine as VM
-from eightdad.core.vm import INSTRUCTION_LENGTH
+from eightdad.core.vm import DEFAULT_EXECUTION_START, INSTRUCTION_LENGTH
 from tests.util import load_and_execute_instruction as load_and_execute
+from tests.util import load_multiple
+
+KEYS_AND_REGISTERS = list(product(range(0,1), range(0,1)))
 
 
 class TestStateSet:
@@ -28,41 +31,42 @@ class TestStateSet:
             vm.release(key)
             assert not vm.pressed(key)
 
-@pytest.mark.parametrize("x,key", list(product(range(0,1), range(0,1))))
+@pytest.mark.parametrize("x,key", KEYS_AND_REGISTERS)
 class TestFX0APauseTillAnyKeypress:
     """
     Tests for waiting for a key press
     """
+    def setup_vm(self, x: int) -> VM:
+        """
+        Helper to create a VM with appropriate instructions loaded.
 
+        This might be better as a fixture once I understand them better.
+        """
+        vm = VM()
+        load_multiple(vm,
+            (0xF00A, {'x':x}),
+            # no-op, store "load 0 to I" as next to be executed
+            0xA000
+        )
+        return vm
+        
     def test_fx0a_pauses_execution(self, x: int, key: int):
         """Ticking the VM after fx0a is executed does not advance state"""
-        vm = VM()
-        start_position = vm.program_counter
-        load_and_execute(vm, 0xF00A,x=x)
-        vm.memory[514] = 0xA0
+        vm = self.setup_vm(x)
         vm.tick(1/20.0)
-        assert vm.program_counter == start_position + INSTRUCTION_LENGTH
+        assert vm.program_counter == DEFAULT_EXECUTION_START + INSTRUCTION_LENGTH
 
     def test_fx0a_resumes_after_keypress(self, x: int, key: int):
         """Execution is resumed after fx0a after a key is pressed"""
-        vm = VM()
-        start_position = vm.program_counter
-        vm.memory[514] = 0xA0
-        load_and_execute(vm, 0xF00A, x=x)
+        vm = self.setup_vm(x)
         vm.press(key)
         vm.tick(1/20.0)
-        assert vm.program_counter == start_position + (INSTRUCTION_LENGTH * 2)
+        assert vm.program_counter == DEFAULT_EXECUTION_START + (INSTRUCTION_LENGTH * 2)
 
     def test_fx0a_sets_register_targeted(self, x: int, key: int):
         """A keypress while waiting sets the register to the requested state"""
-        vm = VM()
-        start_position = vm.program_counter
-        load_and_execute(vm, 0xF00A, x=x)
-        # effectively a no-op, store "load 0 to I" at next instruction to be executed
-        vm.memory[514] = 0xA0
+        vm = self.setup_vm(x)
         vm.press(key)
         vm.tick(1/20.0)
         assert vm.v_registers[x] == key
-
-
 
